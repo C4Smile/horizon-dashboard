a0728769ba21f98f8215a85c13f2c869577401fa
Sito8943 <sito8943@gmail.com>
2024-03-29 08:26:00 -0400

    fixed input types in CustomerForm

--- src/pages/Customers/CustomerForm.jsx
+++ src/pages/Customers/CustomerForm.jsx
@@ -91,7 +91,7 @@ function CustomerForm() {
           render={({ field }) => (
             <TextInput
               {...field}
-              type="name"
+              type="text"
               name="name"
               id="name"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -108,7 +108,7 @@ function CustomerForm() {
           render={({ field }) => (
             <TextInput
               {...field}
-              type="text"
+              type="email"
               name="email"
               id="email"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -159,7 +159,7 @@ function CustomerForm() {
           disabled={customerQuery.isLoading || saving}
           render={({ field }) => (
             <TextInput
-              type="text"
+              type="tel"
               name="phone"
               id="phone"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
