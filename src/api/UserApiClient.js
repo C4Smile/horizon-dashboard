27457dc1934de1e6e19e072d9437b611b7690825
Sito8943 <sito8943@gmail.com>
2024-03-29 09:28:50 -0400

    updated user api client

--- src/api/UserApiClient.js
+++ src/api/UserApiClient.js
@@ -1,13 +1,53 @@
+import { fetchFromLocal, fetchSingleFromLocal, saveToLocal, deleteFromLocal } from "../db/connection";
+
 /**
  * @class UserApiClient
- * @description User API client
+ * @description UserApiClient
  */
 export class UserApiClient {
   /**
    * @description Get all users
-   * @returns Users
+   * @param {string} attributes - Attributes
+   * @returns User list
    */
-  static async getAll() {
-    return [];
+  async getAll(attributes = "*") {
+    return await fetchFromLocal("user", attributes);
+  }
+
+  /**
+   * @description Get user by id
+   * @param {string} id - User id
+   * @param {string} attributes - Attributes
+   * @returns User by id
+   */
+  async getById(id, attributes = "*") {
+    return await fetchSingleFromLocal("user", id, attributes);
+  }
+
+  /**
+   * @description Create user
+   * @param {object} user - User
+   * @returns  Transaction status
+   */
+  async create(user) {
+    return await saveToLocal("user", user);
+  }
+
+  /**
+   * @description Update user
+   * @param {object} user - User
+   * @returns Transaction status
+   */
+  async update(user) {
+    return await saveToLocal("user", user);
+  }
+
+  /**
+   * Remove elements by their id
+   * @param {number[]} ids to delete
+   * @returns Transaction status
+   */
+  async delete(ids) {
+    return await deleteFromLocal("user", ids);
   }
 }
