f1bac2b10a2d7b26e3fbc46bc2344afb09640c71
Sito8943 <sito8943@gmail.com>
2024-03-29 09:29:19 -0400

    added user api client to museum api client

--- src/api/MuseumApiClient.js
+++ src/api/MuseumApiClient.js
@@ -1,5 +1,6 @@
 import { CustomerApiClient } from "./CustomerApiClient";
 import { RoomApiClient } from "./RoomApiClient";
+import { UserApiClient } from "./UserApiClient";
 
 /**
  * @class MuseumApiClient
@@ -10,8 +11,16 @@ export class MuseumApiClient {
    * @description constructor
    */
   constructor() {
+    this.user = new UserApiClient();
     this.customer = new CustomerApiClient();
     this.room = new RoomApiClient();
+  }
+
+  /**
+   * @returns {UserApiClient} Customer
+   */
+  get User() {
+    return this.user;
   }
 
   /**
