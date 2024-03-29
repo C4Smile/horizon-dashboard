e60599f533f9551e6de999e9850ab4d71abeb112
Sito8943 <sito8943@gmail.com>
2024-03-29 08:24:33 -0400

    create RoomApiClient

---
+++ src/api/RoomApiClient.js
@@ -0,0 +1,53 @@
+import { fetchFromLocal, fetchSingleFromLocal, saveToLocal, deleteFromLocal } from "../db/connection";
+
+/**
+ * @class RoomApiClient
+ * @description RoomApiClient
+ */
+export class RoomApiClient {
+  /**
+   * @description Get all users
+   * @param {string} attributes - Attributes
+   * @returns Room list
+   */
+  async getAll(attributes = "*") {
+    return await fetchFromLocal("room", attributes);
+  }
+
+  /**
+   * @description Get user by id
+   * @param {string} id - User id
+   * @param {string} attributes - Attributes
+   * @returns Room by id
+   */
+  async getById(id, attributes = "*") {
+    return await fetchSingleFromLocal("room", id, attributes);
+  }
+
+  /**
+   * @description Create user
+   * @param {object} user - User
+   * @returns  Transaction status
+   */
+  async create(user) {
+    return await saveToLocal("room", user);
+  }
+
+  /**
+   * @description Update user
+   * @param {object} user - User
+   * @returns Transaction status
+   */
+  async update(user) {
+    return await saveToLocal("room", user);
+  }
+
+  /**
+   * Remove elements by their id
+   * @param {number[]} ids
+   * @returns Transaction status
+   */
+  async delete(ids) {
+    return await deleteFromLocal("room", ids);
+  }
+}
