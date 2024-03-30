9ddb1f920f353d9e273331fdb90453f580d654b9
Sito8943 <sito8943@gmail.com>
2024-03-30 18:43:14 -0400

    created Account page

---
+++ src/pages/Account/Account.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PersonalInfo from "./sections/PersonalInfo";
+import Security from "./sections/Security";
+import { useTranslation } from "react-i18next";
+
+/**
+ * Account settings page
+ * @returns Account component
+ */
+function Account() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="p-5 relative">
+      <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-5">
+        {t("_pages:settings.title")}
+      </h1>
+      <PersonalInfo />
+      <Security />
+    </div>
+  );
+}
+
+export default Account;
