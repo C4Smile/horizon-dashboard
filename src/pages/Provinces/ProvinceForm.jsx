9530d158c92604919ee669ff90bf43eae1730aaf
Sito8943 <sito8943@gmail.com>
2024-03-30 14:45:11 -0400

    created Province pages

---
+++ src/pages/Provinces/ProvinceForm.jsx
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useQuery } from "@tanstack/react-query";
+import { useForm, Controller } from "react-hook-form";
+
+// components
+import Loading from "../../partials/loading/Loading";
+import TextInput from "../../components/Forms/TextInput";
+import SelectInput from "../../components/Forms/SelectInput";
+
+// providers
+import { useNotification } from "../../providers/NotificationProvider";
+import { queryClient, useMuseumApiClient } from "../../providers/MuseumApiProvider";
+
+// utils
+import { ReactQueryKeys } from "../../utils/queryKeys";
+
+/**
+ * Province Form page component
+ * @returns Province Form page component
+ */
+function ProvinceForm() {
+  const { id } = useParams();
+
+  const { t } = useTranslation();
+
+  const museumApiClient = useMuseumApiClient();
+
+  const { setNotification } = useNotification();
+  const [saving, setSaving] = useState(false);
+
+  const { handleSubmit, reset, control } = useForm();
+
+  const onSubmit = async (d) => {
+    setNotification("");
+    setSaving(true);
+    try {
+      let result;
+      if (d.id) result = await museumApiClient.Province.create(d);
+      else result = await museumApiClient.Province.update(d);
+      const { error, status } = result;
+      setNotification(String(status));
+
+      // eslint-disable-next-line no-console
+      if (error && error !== null) console.error(error);
+      else if (id !== undefined)
+        queryClient.invalidateQueries({ queryKey: [ReactQueryKeys.Provinces, id] });
+      else reset({});
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+      setNotification(String(e.status));
+    }
+    setSaving(false);
+  };
+
+  const provinceQuery = useQuery({
+    queryKey: [ReactQueryKeys.Provinces, id],
+    queryFn: () => museumApiClient.Province.getById(id),
+    enabled: id !== undefined,
+    retry: false,
+  });
+
+  useEffect(() => {
+    const { error } = provinceQuery;
+    // eslint-disable-next-line no-console
+    if (error && error !== null) console.error(error);
+  }, [provinceQuery]);
+
+  useEffect(() => {
+    if (provinceQuery.data) {
+      const { data } = provinceQuery.data;
+      // eslint-disable-next-line no-console
+      if (data && data !== null) reset({ ...data });
+    }
+  }, [provinceQuery.data, id, reset]);
+
+  const countryQuery = useQuery({
+    queryKey: [ReactQueryKeys.Countries],
+    queryFn: () => museumApiClient.Country.getAll(),
+  });
+
+  return (
+    <div className="px-5 pt-10 flex items-start justify-start">
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-5">
+          {id ? `${t("_pages:provinces.editForm")} ${id}` : t("_pages:provinces.newForm")}
+        </h1>
+        <Controller
+          control={control}
+          disabled={provinceQuery.isLoading || saving}
+          name="name"
+          render={({ field }) => (
+            <TextInput
+              {...field}
+              type="name"
+              name="name"
+              id="name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder={t("_entities:province.name.placeholder")}
+              label={t("_entities:province.name.label")}
+              required
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          disabled={provinceQuery.isLoading || saving}
+          name="iso"
+          render={({ field }) => (
+            <TextInput
+              {...field}
+              type="name"
+              name="iso"
+              id="iso"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder={t("_entities:province.iso.placeholder")}
+              label={t("_entities:province.iso.label")}
+              required
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="country"
+          disabled={provinceQuery.isLoading || countryQuery.isLoading || saving}
+          render={({ field: { onChange, value, ...rest } }) => (
+            <SelectInput
+              {...rest}
+              id="country"
+              name="country"
+              label={t("_entities:customer.country.label")}
+              options={countryQuery.data?.data.map((c) => c.Name) || []}
+              value={value}
+              onChange={(e) => {
+                onChange(e.target.value);
+              }}
+            />
+          )}
+        />
+        <button
+          type="submit"
+          disabled={provinceQuery.isLoading || saving}
+          className="mb-5 relative text-white bg-light-primary transition enabled:hover:bg-primary enabled:focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          {(provinceQuery.isLoading || saving) && (
+            <Loading
+              className="bg-primary w-full h-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-lg "
+              strokeWidth="4"
+              loaderClass="!w-6"
+              color="stroke-white"
+            />
+          )}
+          {t("_accessibility:buttons.submit")}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ProvinceForm;
---
+++ src/pages/Provinces/Provinces.jsx
@@ -0,0 +1,120 @@
+import { useMemo, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useTranslation } from "react-i18next";
+import { useNavigate, Link } from "react-router-dom";
+
+// icons
+import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
+
+// dto
+import { Province } from "../../models/Province";
+
+// utils
+import { extractKeysFromObject } from "../../utils/parser";
+import { ReactQueryKeys } from "../../utils/queryKeys";
+
+// providers
+import { useNotification } from "../../providers/NotificationProvider";
+import { useMuseumApiClient, queryClient } from "../../providers/MuseumApiProvider";
+
+// components
+import Table from "../../components/Table/Table";
+
+/**
+ * Province page
+ * @returns Province page component
+ */
+function Provinces() {
+  const { t } = useTranslation();
+
+  const navigate = useNavigate();
+
+  const { setNotification } = useNotification();
+  const museumApiClient = useMuseumApiClient();
+
+  const preparedColumns = useMemo(() => {
+    const keys = extractKeysFromObject(new Province(), [
+      "id",
+      "dateOfCreation",
+      "lastUpdate",
+      "deleted",
+    ]);
+    return keys.map((key) => ({
+      id: key,
+      label: t(`_entities:province.${key}.label`),
+      className: "",
+    }));
+  }, [t]);
+
+  const provinceQuery = useQuery({
+    queryKey: [ReactQueryKeys.Provinces],
+    queryFn: () => museumApiClient.province.getAll(),
+  });
+
+  const preparedRows = useMemo(() => {
+    if (provinceQuery.data) {
+      const { data } = provinceQuery.data;
+      if (data && data !== null)
+        return data.map((province) => {
+          return {
+            id: province.id,
+            dateOfCreation: new Date(province.dateOfCreation).toLocaleDateString(),
+            lastUpdate: new Date(province.lastUpdate).toLocaleDateString(),
+            deleted: province.deleted
+              ? t("_accessibility:buttons.yes")
+              : t("_accessibility:buttons.no"),
+            name: (
+              <Link className="underline text-light-primary" to={`${province.id}`}>
+                {province.name}
+              </Link>
+            ),
+            country: province.country?.Name,
+          };
+        });
+    }
+  }, [provinceQuery, t]);
+
+  useEffect(() => {
+    const { error } = provinceQuery;
+    // eslint-disable-next-line no-console
+    if (error && error !== null) console.error(provinceQuery.error);
+  }, [provinceQuery]);
+
+  const getActions = [
+    {
+      id: "edit",
+      onClick: (e) => navigate(`/management/provinces/${e.id}`),
+      icon: faPencil,
+      tooltip: t("_accessibility:buttons.edit"),
+    },
+    {
+      id: "delete",
+      onClick: (e) => {
+        const { error, status } = museumApiClient.Customer.delete([e.id]);
+        setNotification(String(status));
+
+        // eslint-disable-next-line no-console
+        if (error && error !== null) console.error(error);
+        else queryClient.invalidateQueries({ queryKey: [ReactQueryKeys.Customers] });
+      },
+      icon: faTrash,
+      tooltip: t("_accessibility:buttons.delete"),
+    },
+  ];
+
+  return (
+    <div className="p-5">
+      <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-5">
+        {t("_pages:management.links.provinces")}
+      </h1>
+      <Table
+        isLoading={provinceQuery.isLoading}
+        rows={preparedRows}
+        columns={preparedColumns}
+        actions={getActions}
+      />
+    </div>
+  );
+}
+
+export default Provinces;
