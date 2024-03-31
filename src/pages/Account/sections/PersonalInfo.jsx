ad67cb77562a1302ee9e685231753c086fe53251
Sito8943 <sito8943@gmail.com>
2024-03-30 21:21:59 -0400

    added logic for security and personal info

--- src/pages/Account/sections/PersonalInfo.jsx
+++ src/pages/Account/sections/PersonalInfo.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
 import { useForm, Controller } from "react-hook-form";
 
 // components
-import Loading from "../../partials/loading/Loading";
-import TextInput from "../../components/Forms/TextInput";
+import Loading from "../../../partials/loading/Loading";
+import TextInput from "../../../components/Forms/TextInput";
 
 // providers
-import { useNotification } from "../../providers/NotificationProvider";
-import { queryClient, useMuseumApiClient } from "../../providers/MuseumApiProvider";
+import { useNotification } from "../../../providers/NotificationProvider";
+import { useMuseumApiClient } from "../../../providers/MuseumApiProvider";
 
 // utils
-import { ReactQueryKeys } from "../../utils/queryKeys";
+import { ReactQueryKeys } from "../../../utils/queryKeys";
+import { fromLocal } from "../../../utils/local";
+import config from "../../../config";
 
 /**
  * Personal Info section
@@ -24,19 +25,45 @@ function PersonalInfo() {
 
   const museumApiClient = useMuseumApiClient();
 
+  const userId = fromLocal(config.user, "object")?.id;
+
   const { setNotification } = useNotification();
   const [saving, setSaving] = useState(false);
 
   const { handleSubmit, reset, control } = useForm();
 
+  const userQuery = useQuery({
+    queryKey: [ReactQueryKeys.Customers, userId],
+    queryFn: () => museumApiClient.Customer.getById(userId),
+    enabled: userId !== undefined,
+    retry: false,
+  });
+
+  useEffect(() => {
+    const { error } = userQuery;
+    // eslint-disable-next-line no-console
+    if (error && error !== null) console.error(error);
+  }, [userQuery]);
+
+  useEffect(() => {
+    if (userQuery.data) {
+      const { data } = userQuery.data;
+      // eslint-disable-next-line no-console
+      if (data && data !== null) reset({ ...data });
+    }
+  }, [userQuery.data, userId, reset]);
+
   const onSubmit = async (d) => {
     setNotification("");
     setSaving(true);
     try {
-      let result;
-      if (d.id) result = await museumApiClient.Customer.create(d);
-      else result = await museumApiClient.Customer.update(d);
-      const { error, status } = result;
+      if (d.password !== d.rPassword) {
+        setSaving(false);
+        // eslint-disable-next-line no-console
+        console.error(t("_accessibility:errors.passwordDoNotMatch"));
+        return setNotification(t("_accessibility:errors.passwordDoNotMatch"));
+      }
+      const { error, status } = await museumApiClient.User.update({ ...d, id: userId });
       setNotification(String(status));
 
       // eslint-disable-next-line no-console
@@ -49,24 +76,120 @@ function PersonalInfo() {
     setSaving(false);
   };
 
-  const accountQuery = useQuery({
-    queryKey: [ReactQueryKeys.Customers, userId],
-    queryFn: () => museumApiClient.Customer.getById(userId),
-    enabled: userId !== undefined,
-    retry: false,
-  });
-
   return (
-    <form className="px-5 pt-10 flex items-start justify-start">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex-col pt-10 flex items-start justify-start">
       <h2 className="text-1xl md:text-2xl text-slate-800 dark:text-slate-100 font-bold mb-5">
         {t("_pages:settings.links.account")}
       </h2>
+      <Controller
+        control={control}
+        disabled={userQuery.isLoading || saving}
+        name="name"
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            type="text"
+            name="name"
+            id="name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.name.placeholder")}
+            label={t("_entities:user.name.label")}
+            required
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="email"
+        disabled={userQuery.isLoading || saving}
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            type="email"
+            name="email"
+            id="email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.email.placeholder")}
+            label={t("_entities:user.email.label")}
+            required
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        disabled={userQuery.isLoading || saving}
+        name="username"
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            type="text"
+            name="username"
+            id="username"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.username.placeholder")}
+            label={t("_entities:user.username.label")}
+            required
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="address"
+        disabled={userQuery.isLoading || saving}
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            type="text"
+            name="address"
+            id="address"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.address.placeholder")}
+            label={t("_entities:user.address.label")}
+            required
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="identification"
+        disabled={userQuery.isLoading || saving}
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            type="text"
+            name="identification"
+            id="identification"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.identification.placeholder")}
+            label={t("_entities:user.identification.label")}
+            required
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="phone"
+        disabled={userQuery.isLoading || saving}
+        render={({ field }) => (
+          <TextInput
+            type="tel"
+            name="phone"
+            id="phone"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.phone.placeholder")}
+            label={t("_entities:user.phone.label")}
+            required
+            {...field}
+          />
+        )}
+      />
       <button
         type="submit"
-        disabled={accountQuery.isLoading || saving}
+        disabled={userQuery.isLoading || saving}
         className="mb-5 relative text-white bg-light-primary transition enabled:hover:bg-primary enabled:focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        {(accountQuery.isLoading || saving) && (
+        {(userQuery.isLoading || saving) && (
           <Loading
             className="bg-primary w-full h-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-lg "
             strokeWidth="4"
--- src/pages/Account/sections/Security.jsx
+++ src/pages/Account/sections/Security.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useForm, Controller } from "react-hook-form";
+
+// components
+import Loading from "../../../partials/loading/Loading";
+import PasswordInput from "../../../components/Forms/PasswordInput";
+
+// providers
+import { useNotification } from "../../../providers/NotificationProvider";
+import { useMuseumApiClient } from "../../../providers/MuseumApiProvider";
+
+// utils
+import { fromLocal } from "../../../utils/local";
+import config from "../../../config";
 
 /**
  * Security section
@@ -8,11 +21,92 @@ import { useTranslation } from "react-i18next";
 function Security() {
   const { t } = useTranslation();
 
+  const museumApiClient = useMuseumApiClient();
+
+  const userId = fromLocal(config.user, "object")?.id;
+
+  const { setNotification } = useNotification();
+  const [saving, setSaving] = useState(false);
+
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = async (d) => {
+    setNotification("");
+    setSaving(true);
+    try {
+      if (d.password !== d.rPassword) {
+        setSaving(false);
+        // eslint-disable-next-line no-console
+        console.error(t("_accessibility:errors.passwordDoNotMatch"));
+        return setNotification(t("_accessibility:errors.passwordDoNotMatch"));
+      }
+      const { error, status } = await museumApiClient.User.update({ ...d, id: userId });
+      setNotification(String(status));
+
+      // eslint-disable-next-line no-console
+      if (error && error !== null) console.error(error);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+      setNotification(String(e.status));
+    }
+    setSaving(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex-col pt-10 flex items-start justify-start">
       <h2 className="text-1xl md:text-2xl text-slate-800 dark:text-slate-100 font-bold mb-5">
         {t("_pages:settings.links.security")}
       </h2>
+      <Controller
+        control={control}
+        disabled={saving}
+        name="password"
+        render={({ field }) => (
+          <PasswordInput
+            {...field}
+            type="text"
+            name="password"
+            id="password"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.password.placeholder")}
+            label={t("_entities:user.password.label")}
+            required
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        disabled={saving}
+        name="rPassword"
+        render={({ field }) => (
+          <PasswordInput
+            {...field}
+            type="text"
+            name="rPassword"
+            id="rPassword"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder={t("_entities:user.rPassword.placeholder")}
+            label={t("_entities:user.rPassword.label")}
+            required
+          />
+        )}
+      />
+      <button
+        type="submit"
+        disabled={saving}
+        className="mb-5 relative text-white bg-light-primary transition enabled:hover:bg-primary enabled:focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        {saving && (
+          <Loading
+            className="bg-primary w-full h-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-lg "
+            strokeWidth="4"
+            loaderClass="!w-6"
+            color="stroke-white"
+          />
+        )}
+        {t("_accessibility:buttons.submit")}
+      </button>
     </form>
   );
 }
