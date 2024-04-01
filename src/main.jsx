f8a1ed1e6830a4614649bd44d638dfaa1db5319c
Sito8943 <sito8943@gmail.com>
2024-04-01 10:06:34 -0400

    added Account Provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+
+// Provider
 import ThemeProvider from "./utils/ThemeContext";
+import { AccountProvider } from "./providers/AccountProvider.jsx";
+
+// App
 import App from "./App";
 
 // i18
@@ -13,7 +18,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <ThemeProvider>
-      <App />
+      <AccountProvider>
+        <App />
+      </AccountProvider>
     </ThemeProvider>
   </Router>,
 );
