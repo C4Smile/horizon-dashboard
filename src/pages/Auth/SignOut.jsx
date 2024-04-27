5727e4c30244cd3fb09416f54a292e1879d54458
Sito8943 <sito8943@gmail.com>
2024-04-27 09:51:23 -0400

    now apiclient in logout

--- src/pages/Auth/SignOut.jsx
+++ src/pages/Auth/SignOut.jsx
@@ -3,25 +3,22 @@ import { useNavigate } from "react-router-dom";
 
 // providers
 import { useAccount } from "../../providers/AccountProvider";
-import { useMuseumApiClient } from "../../providers/MuseumApiProvider";
 
 /**
  * SignOut page
  * @returns SignOut page component
  */
 function SignOut() {
-  const museumApiClient = useMuseumApiClient();
   const { logoutUser } = useAccount();
 
   const navigate = useNavigate();
 
   const logic = useCallback(async () => {
-    await museumApiClient.User.logout();
     logoutUser();
     setTimeout(() => {
       navigate("/auth");
     }, 1000);
-  }, [museumApiClient.User, logoutUser, navigate]);
+  }, [logoutUser, navigate]);
 
   useEffect(() => {
     logic();
