0b5c35ded2c8121c36de4078b7cf5c3018801aad
Sito8943 <sito8943@gmail.com>
2024-07-05 08:31:53 -0400

    created makeRequest method

---
+++ src/db/services.js
@@ -0,0 +1,24 @@
+import config from "../config";
+
+import { fromLocal } from "../utils/local";
+
+/**
+ * @description Make a request to the API
+ * @param {string} url - URL to make the request
+ * @param {string} method - Request method
+ * @param {object} body - Request body
+ * @returns Request response
+ */
+export async function makeRequest(url, method = "GET", body = null) {
+  const request = await fetch(`${config.apiUrl}${url}`, {
+    method,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
+    },
+    body: JSON.stringify(body),
+  });
+  const { error, data, status } = request.json();
+  return { error, data, status };
+}
