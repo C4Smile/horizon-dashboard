caaf6b1fa0ef0a53a247c809faf353f4520ed676
Sito8943 <sito8943@gmail.com>
2024-07-15 09:56:22 -0400

    created push notification api client

---
+++ src/api/PushNotificationApiClient.js
@@ -0,0 +1,84 @@
+import { PushNotification } from "../models/pushNotification/PushNotification";
+
+// services
+import { makeRequest } from "../db/services";
+
+// utils
+import { SortOrder } from "../models/query/GenericFilter";
+
+/**
+ * @class PushNotificationApiClient
+ * @description PushNotificationApiClient
+ */
+export class PushNotificationApiClient {
+  /**
+   * @description Get all pushNotifications
+   * @param {string} sort attribute to order by
+   * @param {string} order asc/desc
+   * @returns {Promise<PushNotification[]>} Countries
+   */
+  async getAll(sort = "lastUpdate", order = SortOrder.ASC) {
+    const { data, error, status } = await makeRequest(`pushNotification?sort=${sort}&order=${order}`);
+    if (error !== null) return { status, statusCode: status, message: error.message };
+    return data;
+  }
+
+  /**
+   * @description Get pushNotification by id
+   * @param {string} id - PushNotification id
+   * @returns {Promise<PushNotification>} PushNotification
+   */
+  async getById(id) {
+    const { data, error, status } = await makeRequest(`pushNotification/${id}`);
+    if (error !== null) return { status, statusCode: status, message: error.message };
+    return data[0];
+  }
+
+  /**
+   * @description Create pushNotification
+   * @param {PushNotification} pushNotification - PushNotification
+   * @param {object} photo - PushNotification photo
+   * @returns {Promise<PushNotification>} PushNotification
+   */
+  async create(pushNotification, photo) {
+    // parsing sent date
+    // parsing sent date
+    pushNotification.sentDate = new Date(pushNotification?.sentDate ?? Date.now()).toISOString();
+    // saving image
+    if (photo) pushNotification.imageId = photo.id;
+    // call service
+    const { error, data, status } = await makeRequest("pushNotification", "POST", pushNotification);
+    if (error !== null) return { status, data, statusCode: status, message: error.message };
+    return { error, status: status === 204 ? 201 : status };
+  }
+
+  /**
+   * @description Update pushNotification
+   * @param {PushNotification} pushNotification - PushNotification
+   * @param {object} photo - PushNotification photo
+   * @returns {Promise<PushNotification>} PushNotification
+   */
+  async update(pushNotification, photo) {
+    // parsing sent date
+    pushNotification.sentDate = new Date(pushNotification?.sentDate ?? Date.now()).toISOString();
+    // saving photo
+    if (photo) pushNotification.imageId = photo.id;
+    // call service
+    const { status, error } = await makeRequest(`pushNotification/${pushNotification.id}`, "PUT", {
+      ...pushNotification,
+      lastUpdate: new Date().toISOString(),
+    });
+    if (error !== null) return { status, statusCode: error.code, message: error.message };
+    return { error, status: status === 204 ? 201 : status };
+  }
+
+  /**
+   * Remove elements by their id
+   * @param {number[]} ids to delete
+   * @returns Transaction status
+   */
+  async delete(ids) {
+    for (const id of ids) await makeRequest(`pushNotification/${id}`, "DELETE");
+    return { status: 204 };
+  }
+}
