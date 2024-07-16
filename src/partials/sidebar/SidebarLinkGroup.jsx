9e4bce0538ae039a5052e6832012140f2d51ceb0
Sito8943 <sito8943@gmail.com>
2024-07-16 10:20:47 -0400

    updated color

--- src/partials/sidebar/SidebarLinkGroup.jsx
+++ src/partials/sidebar/SidebarLinkGroup.jsx
@@ -14,7 +14,7 @@ function SidebarLinkGroup(props) {
   };
 
   return (
-    <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${activeCondition && "bg-slate-400"}`}>
+    <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${activeCondition && "bg-slate-300"}`}>
       {children(handleClick, open)}
     </li>
   );
