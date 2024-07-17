f0f9cf1284017378f9d79b5ffe1ab39420a3bea4
Sito8943 <sito8943@gmail.com>
2024-07-17 10:46:08 -0400

    fixed appTextholder

--- src/pages/AppTexts/AppTextForm.jsx
+++ src/pages/AppTexts/AppTextForm.jsx
@@ -140,7 +140,7 @@ function AppTextForm() {
               name="title"
               id="title"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              appTextholder={t("_entities:appText.title.appTextholder")}
+              placeholder={t("_entities:appText.title.placeholder")}
               label={t("_entities:appText.title.label")}
               required
             />
