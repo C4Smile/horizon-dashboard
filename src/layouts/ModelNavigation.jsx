83f5c1a6058ddcace5cf429a8b6aa8a83efdc011
Sito8943 <sito8943@gmail.com>
2024-07-20 13:48:13 -0400

    disabled on form

--- src/layouts/ModelNavigation.jsx
+++ src/layouts/ModelNavigation.jsx
@@ -28,7 +28,7 @@ function ModelNavigation(props) {
         </Link>
         {!noInsert && (
           <Link
-            disabled={pathname === `/${parent}/${model}/new`}
+            disabled={pathname === `/${parent}/${model}/new` || pathname.match(/^\/[^/]+\/[^/]+\/\d+$/)}
             className={`px-3 py-2 ${
               pathname === `/${parent}/${model}/new` || pathname.match(/^\/[^/]+\/[^/]+\/\d+$/)
                 ? "text-light-primary/40 disabled-link"
