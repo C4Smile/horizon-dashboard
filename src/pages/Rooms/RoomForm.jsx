57c3752e265683a7b67b5839224750c25b75bc17
Sito8943 <sito8943@gmail.com>
2024-07-28 15:21:57 -0400

    number as string or room

--- src/pages/Rooms/RoomForm.jsx
+++ src/pages/Rooms/RoomForm.jsx
@@ -212,7 +212,7 @@ function RoomForm() {
           render={({ field }) => (
             <TextInput
               {...field}
-              type="number"
+              type="string"
               name="number"
               id="number"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-blue-600 peer"
