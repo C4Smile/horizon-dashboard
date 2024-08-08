2dffc2e33c6716a953edc98b3d7587959382c260
Sito8943 <sito8943@gmail.com>
2024-08-08 08:26:24 -0400

    using base api client in tag

--- src/api/TagApiClient.js
+++ src/api/TagApiClient.js
@@ -4,38 +4,25 @@ import { Tag } from "../models/tag/Tag";
 import { makeRequest } from "../db/services";
 
 // utils
-import { SortOrder } from "../models/query/GenericFilter";
 import { fromLocal } from "../utils/local";
 
 // config
 import config from "../config";
 
+// base
+import { BaseAPiClient } from "./utils/BaseApiClient";
+
 /**
  * @class TagApiClient
  * @description TagApiClient
  */
-export class TagApiClient {
+export class TagApiClient extends BaseAPiClient {
   /**
-   * @description Get all tag
-   * @param {string} sort - Sort by
-   * @param {SortOrder} order - Order ASC/DESC
-   * @returns {Promise<Tag[]>} Tag
+   * create base api client
    */
-  async getAll(sort = "lastUpdate", order = SortOrder.ASC) {
-    const { data, error, status } = await makeRequest(`tag?sort=${sort}&order=${order}`);
-    if (error !== null) return { status, error: { message: error.message } };
-    return data;
-  }
-
-  /**
-   * @description Get tag by id
-   * @param {string} id - Tag id
-   * @returns {Promise<Tag>} Tag
-   */
-  async getById(id) {
-    const { data, error, status } = await makeRequest(`tag/${id}`);
-    if (error !== null) return { status, error: { message: error.message } };
-    return data[0];
+  constructor() {
+    super();
+    this.baseUrl = "tag";
   }
 
   /**
@@ -45,7 +32,7 @@ export class TagApiClient {
    */
   async create(tag) {
     // call service
-    const { error, data, status } = await makeRequest("tag", "POST", tag, {
+    const { error, data, status } = await makeRequest(this.baseUrl, "POST", tag, {
       Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
     });
     if (error !== null) return { status, error: { message: error.message } };
@@ -61,7 +48,7 @@ export class TagApiClient {
   async update(tag) {
     // call service
     const { status, error } = await makeRequest(
-      `tag/${tag.id}`,
+      `${this.baseUrl}/${tag.id}`,
       "PATCH",
       {
         ...tag,
@@ -73,29 +60,5 @@ export class TagApiClient {
     );
     if (error !== null) return { status, error: { message: error.message } };
     return { error, status: status === 204 ? 201 : status };
-  }
-
-  /**
-   * Remove elements by their id
-   * @param {number[]} ids to delete
-   * @returns Transaction status
-   */
-  async delete(ids) {
-    const { data, status, error } = await makeRequest(`tag`, "DELETE", ids, {
-      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-    });
-    return { data, error, status: status === 200 ? 204 : status };
-  }
-
-  /**
-   * Restore elements by their id
-   * @param {number[]} ids to restore
-   * @returns Transaction status
-   */
-  async restore(ids) {
-    const { data, status, error } = await makeRequest(`tag/restore`, "PATCH", ids, {
-      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-    });
-    return { data, error, status: status === 200 ? 204 : status };
   }
 }
---
+++ src/api/utils/BaseApiClient.js
@@ -0,0 +1,64 @@
+// services
+import { makeRequest } from "../../db/services";
+
+// utils
+import { SortOrder } from "../../models/query/GenericFilter";
+import { fromLocal } from "../../utils/local";
+
+// config
+import config from "../../config";
+
+/**
+ * @class BaseApiClient
+ * @description it has all base method
+ */
+export class BaseAPiClient {
+  baseUrl = "";
+
+  /**
+   * @description Get all objects
+   * @param {string} sort - Sort by
+   * @param {SortOrder} order - Order ASC/DESC
+   * @returns {Promise<object[]>} Result list
+   */
+  async getAll(sort = "lastUpdate", order = SortOrder.ASC) {
+    const { data, error, status } = await makeRequest(`${this.baseUrl}?sort=${sort}&order=${order}`);
+    if (error !== null) return { status, error: { message: error.message } };
+    return data;
+  }
+
+  /**
+   * @description Get entity by id
+   * @param {string} id - object id
+   * @returns {Promise<object>} object
+   */
+  async getById(id) {
+    const { data, error, status } = await makeRequest(`${this.baseUrl}/${id}`);
+    if (error !== null) return { status, error: { message: error.message } };
+    return data[0];
+  }
+
+  /**
+   * Remove elements by their id
+   * @param {number[]} ids to delete
+   * @returns Transaction status
+   */
+  async delete(ids) {
+    const { data, status, error } = await makeRequest(`${this.baseUrl}`, "DELETE", ids, {
+      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
+    });
+    return { data, error, status: status === 200 ? 204 : status };
+  }
+
+  /**
+   * Restore elements by their id
+   * @param {number[]} ids to restore
+   * @returns Transaction status
+   */
+  async restore(ids) {
+    const { data, status, error } = await makeRequest(`${this.baseUrl}/restore`, "PATCH", ids, {
+      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
+    });
+    return { data, error, status: status === 200 ? 204 : status };
+  }
+}
