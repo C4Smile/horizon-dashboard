bfdb4847154cb3c21ff6252b906f9f33ba578429
Sito8943 <sito8943@gmail.com>
2024-08-10 14:14:05 -0400

    removed delete from user api client

--- src/api/UserApiClient.js
+++ src/api/UserApiClient.js
@@ -146,23 +146,4 @@ export class UserApiClient {
     });
     return request;
   }
-
-  /**
-   * Remove elements by their id
-   * @param {number[]} ids ids to delete
-   * @returns Transaction status
-   */
-  async delete(ids) {
-    for (const id of ids) {
-      await fetch(`user/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-        },
-      });
-    }
-    return { status: 204 };
-  }
 }
