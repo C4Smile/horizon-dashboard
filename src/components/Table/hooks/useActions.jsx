86c91d6ec032a50a1e8b6300f957225a84dde38e
Sito8943 <sito8943@gmail.com>
2024-08-10 18:47:05 -0400

    created useActions hook

---
+++ src/components/Table/hooks/useActions.jsx
@@ -0,0 +1,77 @@
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+// icons
+import { faTrash, faPencil, faArrowRotateLeft } from "@fortawesome/free-solid-svg-icons";
+
+// providers
+import { queryClient } from "../../providers/MuseumApiProvider";
+import { useNotification } from "../../../providers/NotificationProvider";
+
+/**
+ * useActions hook
+ * @param props - hook properties
+ * @returns actions to render
+ */
+export const useActions = (props) => {
+  const { t } = useTranslation();
+
+  const navigate = useNavigate();
+
+  const { setNotification } = useNotification();
+
+  const { parent, apiClient, queryKey, actions = [], canEdit = true, canDelete = true } = props;
+
+  const getActions = useMemo(() => {
+    const toReturn = [...actions];
+    if (canEdit)
+      toReturn.push({
+        id: "edit",
+        onClick: (e) => navigate(`/${parent}/${queryKey}/${e.id}`),
+        icon: faPencil,
+        tooltip: t("_accessibility:buttons.edit"),
+      });
+    if (canDelete)
+      toReturn.push(
+        {
+          id: "delete",
+          hidden: (entity) => entity.deleted.value,
+          onClick: async (e) => {
+            const result = await apiClient.delete([e.id]);
+            const { error, status, data } = result;
+            if (data?.count) {
+              setNotification("deleted", { count: data.count });
+              queryClient.invalidateQueries({ queryKey: [queryKey] });
+            } else {
+              // eslint-disable-next-line no-console
+              console.error(error);
+              setNotification(String(status));
+            }
+          },
+          icon: faTrash,
+          tooltip: t("_accessibility:buttons.delete"),
+        },
+        {
+          id: "restore",
+          hidden: (entity) => !entity.deleted.value,
+          onClick: async (e) => {
+            const result = await apiClient.restore([e.id]);
+            const { error, status, data } = result;
+            if (data?.count) {
+              setNotification("restored", { count: data.count });
+              queryClient.invalidateQueries({ queryKey: [queryKey] });
+            } else {
+              // eslint-disable-next-line no-console
+              console.error(error);
+              setNotification(String(status));
+            }
+          },
+          icon: faArrowRotateLeft,
+          tooltip: t("_accessibility:buttons.restore"),
+        },
+      );
+  }, [actions, apiClient, canDelete, canEdit, navigate, parent, queryKey, setNotification, t]);
+
+  return getActions;
+};
