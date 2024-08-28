f1058a752ea7cb54f74586e8633992a49adc965a
Sito8943 <sito8943@gmail.com>
2024-08-27 22:27:13 -0400

    added tabs and loading to translations

--- src/pages/Applications/Translations.jsx
+++ src/pages/Applications/Translations.jsx
@@ -8,19 +8,36 @@ import { useMuseumApiClient } from "../../providers/MuseumApiProvider";
 import { ReactQueryKeys } from "../../utils/queryKeys";
 
 // components
+import Loading from "../../partials/loading/Loading";
 import TabComponent from "../../components/TabComponent/TabComponent";
 
+/**
+ *
+ * @returns Translations page
+ */
 function Translations() {
   const museumApiClient = useMuseumApiClient();
 
   const { data, isLoading } = useQuery({
     queryKey: [ReactQueryKeys.Applications],
-    queryFn: () => museumApiClient.Application.getAll({ pageSize: 999 }),
+    queryFn: () => museumApiClient.Application.getAll(),
   });
 
   return (
-    <div>
-      <TabComponent />
+    <div className="p-7">
+      {!isLoading ? (
+        <TabComponent
+          tabs={data?.items?.map((app) => ({ id: app.id, label: app.name })) ?? []}
+          content={{}}
+        />
+      ) : (
+        <Loading
+          className="bg-none w-full h-6"
+          strokeWidth="4"
+          loaderClass="!w-6"
+          color="stroke-primary"
+        />
+      )}
     </div>
   );
 }
