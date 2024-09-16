4d35208a445cf75a7b4b94a68d007943f056c772
Sito8943 <sito8943@gmail.com>
2024-09-16 10:56:19 -0400

    loading langauges

--- src/pages/Applications/Translations.jsx
+++ src/pages/Applications/Translations.jsx
@@ -1,4 +1,5 @@
 import { useCallback, useState, useReducer, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
 
 // icons
@@ -6,7 +7,7 @@ import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // api
-import { useMuseumApiClient } from "../../providers/MuseumApiProvider";
+import { queryClient, useMuseumApiClient } from "../../providers/MuseumApiProvider";
 
 // utils
 import { ReactQueryKeys } from "../../utils/queryKeys";
@@ -35,6 +36,8 @@ const translationsReducer = (state, action) => {
  * @returns Translations page
  */
 function Translations() {
+  const { t } = useTranslation();
+
   const museumApiClient = useMuseumApiClient();
 
   const appsQuery = useQuery({
@@ -44,11 +47,23 @@ function Translations() {
 
   const [currentApp, setCurrentApp] = useState(0);
 
+  const [currentLanguage, setCurrentLanguage] = useState(0);
+  const languagesQuery = useQuery({
+    queryKey: [ReactQueryKeys.Language],
+    queryFn: () => museumApiClient.Language.getAll(),
+  });
+
   const translationsQuery = useQuery({
     queryKey: [ReactQueryKeys.Translations, currentApp],
     queryFn: () => museumApiClient.ApplicationTranslation.getByApplication(currentApp),
     enabled: !!currentApp,
   });
+
+  useEffect(() => {
+    if (languagesQuery.data?.items) {
+      setCurrentLanguage(languagesQuery.data?.items[0].id);
+    }
+  }, [languagesQuery.data?.items]);
 
   const [translations, setTranslations] = useReducer(translationsReducer, {});
 
@@ -61,14 +76,13 @@ function Translations() {
         ))}
       </ul>
     );
-
+    console.log(data);
     setTranslations({ type: "add", key: currentApp, component });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentApp, translationsQuery.data]);
 
   const onCSVSelected = useCallback(
     async (event) => {
-      console.log(currentApp);
       const [file] = event.target.files;
       const readFileContent = async (file) =>
         new Promise((resolve, reject) => {
@@ -79,6 +93,10 @@ function Translations() {
         });
       const content = await readFileContent(file);
       const response = await museumApiClient.ApplicationTranslation.uploadFile(content, currentApp);
+
+      if (response.status === 201) {
+        queryClient.invalidateQueries([ReactQueryKeys.Translations, currentApp]);
+      }
     },
     [currentApp, museumApiClient.ApplicationTranslation],
   );
@@ -89,6 +107,19 @@ function Translations() {
 
   return (
     <div className="p-7">
+      <h3 className="mb-5">{t("_pages:translations.languages")}</h3>
+      <ul className="flex gap-5 items-center justify-start w-full">
+        {languagesQuery.data?.items?.map((language) => (
+          <li key={language.id}>
+            <button
+              onClick={() => setCurrentLanguage(language.code)}
+              className={`text-sm  px-5 py-2 ${currentLanguage === language.code ? "bg-slate-200 text-light-primary/60 disabled-link" : "text-primary hover:text-dark-primary"}`}
+            >
+              {language.code}
+            </button>
+          </li>
+        ))}
+      </ul>
       {!appsQuery.isLoading && !translationsQuery.isLoading ? (
         <TabComponent
           onTabChange={(id) => setCurrentApp(id)}
