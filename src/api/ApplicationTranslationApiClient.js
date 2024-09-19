f2a9b36809d81c9c25aad589ba636a24d64c8a15
Sito8943 <sito8943@gmail.com>
2024-09-19 08:43:47 -0400

    added empty line

--- src/api/ApplicationTranslationApiClient.js
+++ src/api/ApplicationTranslationApiClient.js
@@ -81,6 +81,7 @@ export class ApplicationTranslationApiClient extends BaseApiClient {
         Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
       },
     );
+
     if (error !== null) return { status, error: { message: error.message } };
     return { error, status: status === 204 ? 201 : status };
   }
