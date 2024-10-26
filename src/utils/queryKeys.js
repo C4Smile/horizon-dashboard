cacbf19b7c088ca5f6aba2460e3ebcb49b3986d1
Sito8943 <sito8943@gmail.com>
2024-10-26 18:37:31 -0400

    query keys

--- src/utils/queryKeys.js
+++ src/utils/queryKeys.js
@@ -1,42 +1,12 @@
 export const ReactQueryKeys = {
-  Activities: "activities",
-  AppTexts: "appTexts",
-  Events: "events",
-  PushNotifications: "pushNotifications",
   Roles: "roles",
-  Rooms: "rooms",
-  RoomStatuses: "RoomStatuses",
-  RoomTypes: "roomTypes",
-  RoomAreas: "roomAreas",
-  RoomAreaStatuses: "roomAreaStatuses",
-  Services: "services",
+  Buildings: "buildings",
   Users: "users",
-  News: "news",
-  Tags: "tags",
-  GuestBooks: "guestBooks",
-  Languages: "langs",
-  ApplicationTranslations: "appTranslations",
-  Applications: "applications",
-  ChatBot: "chatBot",
-  ChatBotContext: "chatBotContext",
 };
 
-export const entities = ["room", "roomArea", "activity", "events", "news"];
-
-export const chatBotEntities = ["room"];
+export const entities = ["building"];
 
 export const Parents = {
-  room: "museo",
-  roomArea: "museo",
-  guestBook: "museo",
-  service: "museo",
-  roomType: "museo",
-  activity: "informacion",
-  event: "informacion",
-  news: "informacion",
-  tag: "informacion",
-  application: "dispositivos",
-  appText: "gestion",
-  pushNotification: "gestion",
-  user: "personal",
+  building: "game",
+  user: "players",
 };
