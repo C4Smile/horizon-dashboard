243a3ffe420d4c933787543efd76454286d5468d
Sito8943 <sito8943@gmail.com>
2024-10-27 23:02:06 -0400

    index or call components

---
+++ src/pages/Techs/Tabs/index.js
@@ -0,0 +1,6 @@
+import Costs from "./Costs";
+import GeneralInfo from "./GeneralInfo";
+import Produces from "./Produces";
+import TechRequirements from "./TechRequirements";
+
+export { Costs, GeneralInfo, Produces, TechRequirements };
