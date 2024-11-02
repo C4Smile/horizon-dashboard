4de3b1041e1c63e7e61050369fc0b01f73861971
Sito8943 <sito8943@gmail.com>
2024-11-02 11:51:57 -0400

    prepared better resource stuff

--- src/pages/Techs/Tabs/ResourceStuff.jsx
+++ src/pages/Techs/Tabs/ResourceStuff.jsx
@@ -30,9 +30,9 @@ function ResourceStuff(props) {
 
   const [saving, setSaving] = useState(false);
 
-  const { id, label, inputKey, entity, getFunction, queryKey } = props;
+  const { id, label, inputKey, entity, entityToSave, queryFn, saveFn, queryKey } = props;
 
-  const [costs, setCosts] = useReducer((state, action) => {
+  const [lists, setLists] = useReducer((state, action) => {
     const { type } = action;
     switch (type) {
       case "add": {
@@ -45,8 +45,12 @@ function ResourceStuff(props) {
       }
       case "modify": {
         const { item } = action;
-        const found = state.findIndex((jtem) => item.resourceId === jtem.resourceId);
-        if (found >= 0) state[found] = item;
+        const found = state.findIndex((jtem) =>
+          item.attribute === "resource"
+            ? item.value.base === jtem.base
+            : item.value.resourceId === jtem.resourceId,
+        );
+        if (found >= 0) state[found] = item.value;
         return [...state];
       }
       case "delete": {
@@ -62,13 +66,13 @@ function ResourceStuff(props) {
 
   const costQuery = useQuery({
     queryKey,
-    queryFn: getFunction,
-    enabled: !!getFunction && !!queryKey,
+    queryFn,
+    enabled: !!queryFn && !!queryKey,
   });
 
   useEffect(() => {
     const result = costQuery.data;
-    if (result) setCosts({ type: "set", items: result?.items });
+    if (result) setLists({ type: "set", items: result?.items });
   }, [costQuery.data]);
 
   const resourcesQuery = useQuery({
@@ -90,35 +94,35 @@ function ResourceStuff(props) {
   const save = useCallback(async () => {
     setSaving(true);
     try {
-      const result = await horizonApiClient.Tech.saveCosts(id, costs);
+      const result = await saveFn(id, lists);
 
       const { error, status } = result;
-      setNotification(String(status), { model: t("_entities:entities.techCost") });
+      setNotification(String(status), { model: t(`_entities:entities.${entityToSave}`) });
 
       // eslint-disable-next-line no-console
       if (error && error !== null) console.error(error.message);
       else {
-        queryClient.invalidateQueries({ queryKey: [ReactQueryKeys.TechCosts, id] });
+        queryClient.invalidateQueries({ queryKey: [queryKey] });
       }
     } catch (e) {
       // eslint-disable-next-line no-console
       console.error(e);
-      setNotification(String(e.status), { model: t("_entities:entities.techCost") });
+      setNotification(String(e.status), { model: t(`_entities:entities.${entityToSave}`) });
     }
     setSaving(false);
-  }, [costs, horizonApiClient, id, setNotification, t]);
+  }, [lists, entityToSave, id, queryKey, saveFn, setNotification, t]);
 
   return (
     <div className="form mt-5 gap-5 w-full">
-      {costs?.map((cost, i) => (
+      {lists?.map((cost, i) => (
         <ResourceForm
           value={cost}
           resources={resourcesList}
-          key={cost.resource ?? i}
-          label={`${t(`_entities:${entity}.resource.${label}`)} ${i + 1}`}
+          key={i}
+          label={`${t(`_entities:${entity}.resource.${label}`)}`}
           inputLabel={t(`_entities:base.${inputKey}.label`)}
           inputPlaceholder={t(`_entities:base.${inputKey}.placeholder`)}
-          onChange={(value) => setCosts({ type: "modify", item: { value } })}
+          onChange={(value, attribute) => setLists({ type: "modify", item: { value, attribute } })}
         />
       ))}
       <div className="flex gap-3 absolute bottom-6 left-6">
@@ -135,14 +139,14 @@ function ResourceStuff(props) {
           )}
         </button>
         <button
-          disabled={saving || costs.length >= resourcesList.length}
+          disabled={saving || lists.length >= resourcesList.length}
           onClick={() =>
-            setCosts({
+            setLists({
               type: "add",
-              item: { resourceId: resourcesList[0].id, baseCost: 1, factor: 1 },
+              item: { resourceId: resourcesList[0].id, base: 1, factor: 1 },
             })
           }
-          className={`${costs.length >= resourcesList.length ? "bg-ocean/80 text-white/60" : "bg-ocean text-white"} w-10 h-10 rounded-full`}
+          className={`${lists.length >= resourcesList.length ? "bg-ocean/80 text-white/60" : "bg-ocean text-white"} w-10 h-10 rounded-full`}
         >
           <FontAwesomeIcon icon={faAdd} />
         </button>
