5ae4716bc12d41fee0811c50cc6a96856caafbe0
Sito8943 <sito8943@gmail.com>
2024-11-03 16:52:46 -0500

    reduced all tech many api clients

--- src/api/TechProducesApiClient.js
+++ src/api/TechProducesApiClient.js
@@ -1,60 +1,16 @@
-// services
-import { makeRequest } from "../db/services";
-
-// utils
-import { fromLocal } from "../utils/local";
-
-// config
-import config from "../config";
+// base
+import { BaseManyApiClient } from "./utils/BaseManyApiClient";
+import { Tech } from "../models/tech/Tech";
 
 /**
  * @class TechProducesApiClient
  * @description TechProducesApiClient
  */
-export class TechProducesApiClient {
+export class TechProducesApiClient extends BaseManyApiClient {
   /**
-   * @param {number} techId id of the tech
-   * @returns tech produces
+   * constructor
    */
-  async get(techId) {
-    // call service
-    const { error, data, status } = await makeRequest(`techProduces/${techId}`, "GET", null, {
-      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-    });
-
-    return { error, items: data, status: status === 204 ? 201 : status };
-  }
-
-  /**
-   * @description Create techProduces
-   * @param {number} techId tech to save
-   * @param {object} techProduces - TechProduces
-   * @returns  Transaction status
-   */
-  async create(techId, techProduces) {
-    // call service
-    const { error, data, status } = await makeRequest(`techProduces/${techId}`, "POST", techProduces, {
-      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-    });
-
-    return { error, data, status: status === 204 ? 201 : status };
-  }
-
-  /**
-   * @description Get a techProduces by newsId
-   * @param {number} tagId - Tag id
-   * @param {number} newsId - News id
-   * @returns Status
-   */
-  async delete(tagId, newsId) {
-    await makeRequest(
-      `techProduces`,
-      "DELETE",
-      { tagId, newsId },
-      {
-        Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-      },
-    );
-    return { status: 204 };
+  constructor() {
+    super(Tech.resourceUpgrade, "techId", "resourceId");
   }
 }
--- src/api/TechReqTechsApiClient.js
+++ src/api/TechReqTechsApiClient.js
@@ -1,60 +1,16 @@
-// services
-import { makeRequest } from "../db/services";
-
-// utils
-import { fromLocal } from "../utils/local";
-
-// config
-import config from "../config";
+// base
+import { BaseManyApiClient } from "./utils/BaseManyApiClient";
+import { Tech } from "../models/tech/Tech";
 
 /**
  * @class TechReqTechsApiClient
  * @description TechReqTechsApiClient
  */
-export class TechReqTechsApiClient {
+export class TechReqTechsApiClient extends BaseManyApiClient {
   /**
-   * @param {number} techId id of the tech
-   * @returns tech requirements
+   * constructor
    */
-  async get(techId) {
-    // call service
-    const { error, data, status } = await makeRequest(`techReqTechs/${techId}`, "GET", null, {
-      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-    });
-
-    return { error, items: data, status: status === 204 ? 201 : status };
-  }
-
-  /**
-   * @description Create techReqTechs
-   * @param {number} techId tech to save
-   * @param {object} techReqTechs - TechReqTechs
-   * @returns  Transaction status
-   */
-  async create(techId, techReqTechs) {
-    // call service
-    const { error, data, status } = await makeRequest(`techReqTechs/${techId}`, "POST", techReqTechs, {
-      Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-    });
-
-    return { error, data, status: status === 204 ? 201 : status };
-  }
-
-  /**
-   * @description Get a techReqTechs by newsId
-   * @param {number} tagId - Tag id
-   * @param {number} newsId - News id
-   * @returns Status
-   */
-  async delete(tagId, newsId) {
-    await makeRequest(
-      `techReqTechs`,
-      "DELETE",
-      { tagId, newsId },
-      {
-        Authorization: "Bearer " + fromLocal(config.user, "object")?.token,
-      },
-    );
-    return { status: 204 };
+  constructor() {
+    super(Tech.techRequirement, "techId", "techReqId");
   }
 }
