8f51f4383972e0be385c9bb2bdf9a71df0643c31
Sito8943 <sito8943@gmail.com>
2024-11-14 18:20:17 -0500

    Added BuildingType entity

---
+++ src/models/buildingType/BuildingType.js
@@ -0,0 +1,24 @@
+import { Entity } from "../entity/Entity";
+
+/**
+ * @class BuildingType
+ * @description Represents a buildingType
+ */
+export class BuildingType extends Entity {
+  name = "";
+  urlName = "";
+  imageId = 0;
+
+  /**
+   * @param {number} id - BuildingType id
+   * @param {string} name - BuildingType name
+   * @param {Date} dateOfCreation - BuildingType date of creation
+   * @param {Date} lastUpdate - BuildingType last update
+   * @param {boolean} deleted - BuildingType deleted
+   */
+  constructor(id, name, dateOfCreation = new Date(), lastUpdate = new Date(), deleted = false) {
+    super(id, dateOfCreation, lastUpdate, deleted);
+    this.name = name;
+    BuildingType.className = "buildingType";
+  }
+}
