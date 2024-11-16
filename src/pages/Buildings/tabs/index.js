ec2899f0c1027db21f18455883949b1b5d86e050
Sito8943 <sito8943@gmail.com>
2024-11-16 09:09:20 -0500

    fixed tabs removed deleted files

--- src/pages/Buildings/tabs/index.js
+++ src/pages/Buildings/tabs/index.js
@@ -1,6 +1,4 @@
 import GeneralInfo from "./GeneralInfo";
 import ResourceStuff from "../../../components/ResourceStuff/ResourceStuff";
-import TechsStuff from "../../../components/TechStuff/TechStuff";
-import BuildingStuff from "../../../components/BuildingStuff/BuildingStuff";
 
-export { GeneralInfo, ResourceStuff, TechsStuff, BuildingStuff };
+export { GeneralInfo, ResourceStuff };
