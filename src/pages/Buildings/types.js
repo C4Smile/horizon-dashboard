19bf900116548a4043f0d553da15387dfcbf2457
Sito <sito8943@gmail.com>
2024-11-17 18:54:37 -0500

    added hide condition on tab for building

--- src/pages/Buildings/types.js
+++ src/pages/Buildings/types.js
@@ -1,8 +1,8 @@
 export const buildingTabs = [
-  "general",
-  "produces",
-  "costs",
-  "upkeep",
-  "buildingReqTechs",
-  "buildingReqBuildings",
+  { id: "general" },
+  { id: "produces", hide: (condition) => condition },
+  { id: "costs", hide: (condition) => condition },
+  { id: "upkeep", hide: (condition) => condition },
+  { id: "buildingReqTechs", hide: (condition) => condition },
+  { id: "buildingReqBuildings", hide: (condition) => condition },
 ];
