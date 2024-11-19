85796d2a276c8d595b148783a2aabf0cd1ddeff9
Sito <sito8943@gmail.com>
2024-11-19 18:16:24 -0500

    created skill entity

---
+++ src/models/skill/Skill.js
@@ -0,0 +1,32 @@
+import { Entity } from "../entity/Entity";
+
+/**
+ * @class Skill
+ * @description Represents a skill
+ */
+export class Skill extends Entity {
+  name = "";
+  urlName = "";
+  description = "";
+  imageId = 0;
+
+  static className = "skill";
+
+  /**
+   * @param {number} id - Skill id
+   * @param {string} name - Skill name
+   * @param {Date} dateOfCreation - Skill date of creation
+   * @param {Date} lastUpdate - Skill last update
+   * @param {boolean} deleted - Skill deleted
+   */
+  constructor(
+    id = 0,
+    name = "",
+    dateOfCreation = new Date(),
+    lastUpdate = new Date(),
+    deleted = false,
+  ) {
+    super(id, dateOfCreation, lastUpdate, deleted);
+    this.name = name;
+  }
+}
