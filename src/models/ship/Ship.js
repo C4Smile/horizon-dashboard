339ab233bc4533f5a794cb2832fbf92f908c303a
Sito <sito8943@gmail.com>
2024-11-20 22:03:53 -0500

    fixed buildingRequirement

--- src/models/ship/Ship.js
+++ src/models/ship/Ship.js
@@ -18,7 +18,7 @@ export class Ship extends Entity {
   static costs = "shipCosts";
   static upkeeps = "shipUpkeeps";
   static techRequirement = "shipReqTechs";
-  static buildRequirement = "shipReqBuildings";
+  static buildingRequirement = "shipReqBuildings";
 
   /**
    * @param {number} id - Ship id
