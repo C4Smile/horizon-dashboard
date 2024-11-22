31c37ace07dc5d20817df92bd87baa164cb204a0
Sito <sito8943@gmail.com>
2024-11-22 11:11:56 -0500

    created cannon entity

---
+++ src/models/cannon/Cannon.js
@@ -0,0 +1,38 @@
+import { Entity } from "../entity/Entity";
+
+/**
+ * @class Cannon
+ * @description Represents a ship
+ */
+export class Cannon extends Entity {
+  name = "";
+  baseDamage = 0;
+  weight = 0;
+  creationTime = 0;
+  urlName = "";
+  description = "";
+  image = {};
+
+  static className = "ship";
+  static costs = "shipCosts";
+  static techRequirement = "shipReqTechs";
+  static buildingRequirement = "shipReqBuildings";
+
+  /**
+   * @param {number} id - Ship id
+   * @param {string} name - Ship name
+   * @param {Date} dateOfCreation - Ship date of creation
+   * @param {Date} lastUpdate - Ship last update
+   * @param {boolean} deleted - Ship deleted
+   */
+  constructor(
+    id = 0,
+    name = "",
+    dateOfCreation = new Date(),
+    lastUpdate = new Date(),
+    deleted = false,
+  ) {
+    super(id, dateOfCreation, lastUpdate, deleted);
+    this.name = name;
+  }
+}
