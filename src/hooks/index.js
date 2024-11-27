7da82cb1e411af90651dc6101c3742754b0e8ddf
Sito <sito8943@gmail.com>
2024-11-27 10:45:33 -0500

    created index for all imports in hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+import { useRestoreAction } from "./baseActions/useRestoreAction.jsx";
+import { useEditAction } from "./baseActions/useEditAction.jsx";
+import { useDeleteAction } from "./baseActions/useDeleteAction.jsx";
+
+export { useRestoreAction, useEditAction, useDeleteAction };
