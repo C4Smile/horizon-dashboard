9960b9611230f908ae9d57f38cd819da0847a6e9
Sito <sito8943@gmail.com>
2024-11-27 19:04:56 -0500

    using new action approach in building type

--- src/pages/BuildingTypes/BuildingTypes.jsx
+++ src/pages/BuildingTypes/BuildingTypes.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -9,6 +9,9 @@ import { Table, useTableOptions } from "@sito/dashboard";
 // images
 import noProduct from "../../assets/images/no-product.jpg";
 
+// icons
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+
 // dto
 import { BuildingType } from "../../models/buildingType/BuildingType";
 
@@ -18,11 +21,14 @@ import { ReactQueryKeys } from "../../utils/queryKeys";
 import { staticUrlPhoto } from "../../components/utils";
 import { useParseColumns, useParseRows } from "../../utils/parseBaseColumns.jsx";
 
+// components
+import { FloatingButton } from "../../components/FloatingButton/FloatingButton.jsx";
+
 // providers
 import { useHorizonApiClient } from "../../providers/HorizonApiProvider";
 
 // hooks
-import { useActions } from "../../hooks/useActions";
+import { useRestoreAction, useDeleteAction, useEditAction } from "../../hooks";
 
 const columnClasses = {
   lastUpdate: "w-56",
@@ -73,10 +79,28 @@ function BuildingTypePage() {
     };
   };
 
-  const getActions = useActions({
-    apiClient: horizonApiClient.BuildingType,
-    queryKey: ReactQueryKeys.BuildingTypes,
+  //#region Actions
+
+  const editAction = useEditAction({
+    entity: ReactQueryKeys.BuildingTypes,
   });
+
+  const restoreAction = useRestoreAction({
+    entity: ReactQueryKeys.BuildingTypes,
+    apiClient: horizonApiClient.BuildingType,
+  });
+
+  const deleteAction = useDeleteAction({
+    entity: ReactQueryKeys.BuildingTypes,
+    apiClient: horizonApiClient.BuildingType,
+  });
+
+  const getActions = useCallback(
+    (row) => [editAction.action(row), restoreAction.action(row), deleteAction.action(row)],
+    [deleteAction, editAction, restoreAction],
+  );
+
+  //#endregion Actions
 
   const { columns } = useParseColumns(
     extractKeysFromObject(new BuildingType(), ["id", "dateOfCreation", "deleted", "urlName"]),
@@ -86,16 +110,19 @@ function BuildingTypePage() {
   const { rows } = useParseRows(prepareRows);
 
   return (
-    <Table
-      rows={data?.items}
-      actions={getActions}
-      isLoading={isLoading}
-      parseRows={rows}
-      entity={BuildingType.className}
-      columns={columns}
-      columnsOptions={{ columnClasses, noSortableColumns }}
-      title={t("_pages:game.links.buildingTypes")}
-    />
+    <>
+      <Table
+        rows={data?.items}
+        actions={getActions}
+        isLoading={isLoading}
+        parseRows={rows}
+        entity={BuildingType.className}
+        columns={columns}
+        columnsOptions={{ columnClasses, noSortableColumns }}
+        title={t("_pages:game.links.buildingTypes")}
+      />
+      <FloatingButton component="link" href="new" icon={faAdd} />
+    </>
   );
 }
 
