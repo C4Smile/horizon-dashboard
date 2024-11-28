da28be3002542f5a867478833a59b24d57b18c30
Sito <sito8943@gmail.com>
2024-11-27 19:15:22 -0500

    using new action approach in ships

--- src/pages/Ships/Ships.jsx
+++ src/pages/Ships/Ships.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -28,7 +28,7 @@ import { useParseColumns, useParseRows } from "../../utils/parseBaseColumns.jsx"
 import { useHorizonApiClient } from "../../providers/HorizonApiProvider";
 
 // hooks
-import { useActions } from "../../hooks/useActions";
+import { useRestoreAction, useDeleteAction, useEditAction } from "../../hooks";
 
 const columnClasses = {
   lastUpdate: "w-44",
@@ -101,11 +101,28 @@ function ShipPage() {
     };
   };
 
-  const getActions = useActions({
-    apiClient: horizonApiClient.Ship,
-    queryKey: ReactQueryKeys.Ships,
-    parent: Parents.game,
+  //#region Actions
+
+  const editAction = useEditAction({
+    entity: ReactQueryKeys.Ships,
   });
+
+  const restoreAction = useRestoreAction({
+    entity: ReactQueryKeys.Ships,
+    apiClient: horizonApiClient.Ship,
+  });
+
+  const deleteAction = useDeleteAction({
+    entity: ReactQueryKeys.Ships,
+    apiClient: horizonApiClient.Ship,
+  });
+
+  const getActions = useCallback(
+    (row) => [editAction.action(row), restoreAction.action(row), deleteAction.action(row)],
+    [deleteAction, editAction, restoreAction],
+  );
+
+  //#endregion Actions
 
   const { columns } = useParseColumns(
     extractKeysFromObject(new Ship(), ["id", "dateOfCreation", "deleted", "description", "urlName"]),
